7c7843ce186d67e56c40055e6df0ce0465dfb83b
Daniel Größinger <daniel.groessinger@gmail.com>
2024-09-15 16:15:16 +0200

    add bluetooth permissions

--- Protogen Bluetooth App/MainPage.xaml.cs
+++ Protogen Bluetooth App/MainPage.xaml.cs
@@ -10,7 +10,23 @@
 
         private void BTScanBtn_Clicked(object sender, EventArgs e)
         {
+#if ANDROID
+            var enable = new Android.Content.Intent(Android.Bluetooth.BluetoothAdapter.ActionRequestEnable);
+            enable.SetFlags(Android.Content.ActivityFlags.NewTask);
 
+            var bluetoothManager = (Android.Bluetooth.BluetoothManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.BluetoothService);
+            var bluetoothAdapter = bluetoothManager.Adapter;
+
+            if (bluetoothAdapter.IsEnabled == true)
+            {
+                // todo: scan for devices
+            }
+            else
+            {
+                // Enable the Bluetooth
+                Android.App.Application.Context.StartActivity(enable);
+            }
+#endif
         }
     }
 
